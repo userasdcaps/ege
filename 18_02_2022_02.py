d2d4a2e49401f23679ff395dd1774fc2fb9d9bf0
userasdcaps <100302252+userasdcaps@users.noreply.github.com>
2022-02-23 19:32:55 +0300

    9

--- 18_02_2022_02.py
+++
@@ -1,7 +0,0 @@
-for a in range(2):
-    for b in range(2):
-        for c in range(2):
-            for d in range(2):
-                for e in range(2):
-                    if (e and (((not(b) or c) and (not(c) or d)) == (b or a))) == 1:
-                        print(a, b, c, d, e)
